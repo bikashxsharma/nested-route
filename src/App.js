9292041f4e82291b5d4e86b51a1982b87c7074ab
Bikash <36207673+bikashxsharma@users.noreply.github.com>
2020-08-04 22:27:22 +0300

    nested route upto 3 level

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link, useLocation, useParams } from 'react-router-dom'
+import posts from './postData.js'
 import './App.css';
+
+
+
+
+const Home = () => {
+  return (<h1>Home</h1>)
+}
+const Body = () => {
+  const { postId } = useParams()
+  const post = posts.filter((post) => post.id === parseInt(postId))[0]
+  return (<p>{post.body}</p>)
+}
+const Post = () => {
+  const { postId } = useParams()
+  const location = useLocation()
+  console.log(location)
+  const post = posts.filter((post) => post.id === parseInt(postId))[0]
+
+  return (<Router>
+    <h2>{post.title}</h2>
+    <p><Link to={`${location.pathname}/${post.id}`}>Read more..</Link></p>
+    <Route path={`${location.pathname}/:postId`} component={Body} />
+  </Router>
+  )
+}
+
+const Posts = () => {
+  return (
+    <Router>
+      <h1>Post</h1>
+      <ul>
+        {posts.map(({ title, id }) => (
+          <li key={id}><Link to={`/posts/${id}`}>{title}</Link></li>
+        ))}
+      </ul>
+      <hr />
+      <Route path={`/posts/:postId`} component={Post} />
+    </Router>)
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/posts">Posts</Link></li>
+        </ul>
+        <hr />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/posts' component={Posts} />
+      </div>
+    </Router>
   );
 }
 
